0b3bb31d218401c8f7be855df4e3cc4877e92b99
Pablo Romero <75594450+RomeroPablo@users.noreply.github.com>
2023-10-21 14:00:15 -0500

    Update IMU.h
    Removed magnetometer from IMU data struct.

--- Drivers/Periphs/Inc/IMU.h
+++ Drivers/Periphs/Inc/IMU.h
@@ -24,9 +24,6 @@ typedef struct {
     int16_t accel_x;
     int16_t accel_y;
     int16_t accel_z;
-    int16_t mag_x;
-    int16_t mag_y;
-    int16_t mag_z;
     int16_t gyr_x;
     int16_t gyr_y;
     int16_t gyr_z;
