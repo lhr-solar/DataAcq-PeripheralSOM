1466ade970e6c1d3e2c323f3621dff98ffef9ec1
pablo <romeropablojr03@gmail.com>
2023-11-25 04:15:32 -0600

    added Read Data from hardware FIFO

--- Drivers/Periphs/Src/IMU.c
+++ Drivers/Periphs/Src/IMU.c
@@ -45,7 +45,7 @@ BaseType_t IMU_Init(void) {
   // NOTE: FIFO size, rate and mode may be further configured, see IMU.h for more details
   for(int i = 0; i < fifoInit[0], i ++){
     pTxData = fifoInit[i+1];
-    HAL_SPI_Transmit(&hspi, &pTxData, 1, NO_POLLING);
+    HAL_SPI_Transmit(&hspi1, &pTxData, 1, NO_POLLING);
   }
 }
 
@@ -57,41 +57,70 @@ BaseType_t IMU_Init(void) {
  * @return pdTRUE if data was successfully fetched from queue, pdFALSE if queue is empty
  */
 BaseType_t IMU_ReadData(IMUData_t *Data) {
+  int pTxData = 0;
+  int pRxData_L = 0;
+  int pRxData_H = 0;
+  for(int i = 0; i < readDataReg[0] - 1; i = i + 2){
+    pTxData = readDataReg[i+1];
+    HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData1, 1, NO_POLLING);
+    pTxData = readDataReg[i+2];
+    HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData2, 1, NO_POLLING);
+    switch(i){
+      case 0: Data->gyr_x = (pRxData1 << 8) | pRxData2; break;
+      case 2: Data->gyr_y = (pRxData1 << 8) | pRxData2; break;
+      case 4: Data->gyr_z = (pRxData1 << 8) | pRxData2; break;
+      case 6: Data->accel_x = (pRxData1 << 8) | pRxData2; break;
+      case 8: Data->accel_y = (pRxData1 << 8) | pRxData2; break;
+      case 10: Data->accel_z = (pRxData1 << 8) | pRxData2; break;
+     }
+   }
+
+}
+
+/** IMU Read Data
+ * 
+ * @brief Update IMU struct with new data fetched from hardware FIFO
+ * 
+ * @param Data : Pointer to struct used to collect IMU Data
+ * @return pdTRUE if data was successfully fetched from queue, pdFALSE if queue is empty
+ */
+BaseType_t IMU_ReadDataHWQueue(IMUData_t *Data){
   // TODO: test on hardware
+  // behavior unpredictable until thourougly tested on hardware, recommend using IMU Read Data at the moment
   int pTxData = 0;
   int pRxData1 = 0;
   int pRxData2 = 0;
 
   pTxData = LSM6_FIFO_stored; /*check if FIFO empty*/
-  HAL_SPI_TransmitReceive(&hspi, &pTxData, &pRxData1, 1, NO_POLLING);
+  HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData1, 1, NO_POLLING);
   if(pRxData1 == 0){
     return pdFalse;
   }
   pTxData = LSM6_FIFO_read_tag;  /*get tag*/
-  HAL_SPI_TransmitReceive(&hspi, &pTxData, &pRxData1, 1, NO_POLLING);
+  HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData1, 1, NO_POLLING);
 
   if(pRxData == LSM6_Gyroscope_NC){ // may be the wrong FIFO tag, assumes gyroscope
-    for(int i = 0; i < fifoXYZregisters[0] - 1; i = i + 2){
-      pTxData = fifoXYZregisters[i+1];
-      HAL_SPI_TransmitReceive(&hspi, &pTxData, &pRxData1, 1, NO_POLLING);
-     pTxData = fifoXYZregisters[i+2];
-     HAL_SPI_TransmitReceive(&hspi, &pTxData, &pRxData2, 1, NO_POLLING);
+    for(int i = 0; i < fifoQXYZregisters[0] - 1; i = i + 2){
+      pTxData = fifoQXYZregisters[i+1];
+      HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData1, 1, NO_POLLING);
+     pTxData = fifoQXYZregisters[i+2];
+     HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData2, 1, NO_POLLING);
      switch(i){
-       case 0: Data->gyr_x = (pRxData1 << 8) | pRxData2;
-       case 2: Data->gyr_y = (pRxData1 << 8) | pRxData2;
-       case 4: Data->gyr_z = (pRxData1 << 8) | pRxData2;
+       case 0: Data->gyr_x = (pRxData1 << 8) | pRxData2; break;
+       case 2: Data->gyr_y = (pRxData1 << 8) | pRxData2; break;
+       case 4: Data->gyr_z = (pRxData1 << 8) | pRxData2; break;
      }
    }
   } else if(pRxData == LSM6_Accelerometer_NC){ // may be the wrong FIFO tag, assumes accelerometer
-    for(int i = 0; i < fifoXYZregisters[0] - 1; i = i + 2){
-      pTxData = fifoXYZregisters[i+1];
-      HAL_SPI_TransmitReceive(&hspi, &pTxData, &pRxData1, 1, NO_POLLING);
-     pTxData = fifoXYZregisters[i+2];
-     HAL_SPI_TransmitReceive(&hspi, &pTxData, &pRxData2, 1, NO_POLLING);
+    for(int i = 0; i < fifoQXYZregisters[0] - 1; i = i + 2){
+      pTxData = fifoQXYZregisters[i+1];
+      HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData1, 1, NO_POLLING);
+     pTxData = fifoQXYZregisters[i+2];
+     HAL_SPI_TransmitReceive(&hspi1, &pTxData, &pRxData2, 1, NO_POLLING);
      switch(i){
-       case 0: Data->accel_x = (pRxData1 << 8) | pRxData2;
-       case 2: Data->accel_y = (pRxData1 << 8) | pRxData2;
-       case 4: Data->accel_z = (pRxData1 << 8) | pRxData2;
+       case 0: Data->accel_x = (pRxData1 << 8) | pRxData2; break;
+       case 2: Data->accel_y = (pRxData1 << 8) | pRxData2; break;
+       case 4: Data->accel_z = (pRxData1 << 8) | pRxData2; break;
      }
    }
   } else {
